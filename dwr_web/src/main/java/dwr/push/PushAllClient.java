fc4f00c0ac8838fadc508b21ca16ac84cc3e8a6b
wujk <wujk@DESKTOP-5K2IHCV>
2018-07-06 00:34:21 +0800

    modify

--- dwr_web/src/main/java/dwr/push/PushAllClient.java
+++ dwr_web/src/main/java/dwr/push/PushAllClient.java
@@ -2,7 +2,7 @@ package dwr.push;
 
 import dwr.PushAll;
 
-public class PushAllClient {
+public class PushAllClient { 
 	public void pushAll(String funcName, String msg) {
 		new PushAll().sendAll(funcName, msg);
 	}
